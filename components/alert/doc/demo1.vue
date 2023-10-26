008495b6844c509e67c52564a7f6ed2b015a6c9f
hgh <1006676534@qq.com>
2023-10-26 11:32:40 +0800

    deploy

---
+++ components/alert/doc/demo1.vue
@@ -0,0 +1,6 @@
+<template>
+  <h-alert title="成功提示的文案" type="success"></h-alert>
+  <h-alert title="消息提示的文案" type="info"></h-alert>
+  <h-alert title="警告提示的文案" type="warning"></h-alert>
+  <h-alert title="错误提示的文案" type="error"></h-alert>
+</template>
